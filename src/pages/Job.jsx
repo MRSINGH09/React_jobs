119447fd026beeda191e1183f851ccca6a48b594
abhishek kumar <abhisheksingh3586@gmail.com>
2025-01-16 21:50:19 +0530

    fix:grid column length

--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -35,8 +35,8 @@ function Job({deleteJob}) {
         </div>
       </section>
 
-      <section className='w-fit bg-indigo-50'>
-        <div className='container m-auto py-10 px-6 w-[85%]'>
+      <section className='bg-indigo-50'>
+        <div className='container m-auto py-10 px-6'>
           <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
             <main>
               <div className='bg-white p-6 rounded-lg shadow-md text-center md:text-left'>
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 import BrowseJobs from '../components/BrowseJobs'
 
 function Jobs() {
   return (
-    <>
+    <section className='bg-blue-50 px-4 py-6'>
     <BrowseJobs/>
-    </>
+    </section>
   )
 }
 
